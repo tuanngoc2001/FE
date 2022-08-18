f21bd5d1e7082b6a6722b7dbf5d1b20e07b41298
tuanngoc2001 <ngoccnttk60@gmail.com>
2022-08-18 16:06:28 +0700

    18/08 done base typescript

---
+++ tscode/doccument/index.js
@@ -0,0 +1,52 @@
+var user = {
+    name: "Hayes",
+    id: 0
+};
+console.log(user);
+function union(code) {
+    console.log("type ".concat(code));
+}
+union("string");
+union(1);
+///generics
+// interface Backpack<Type> {
+//     add: (obj: Type) => void;
+//     get: () => Type;
+//   }
+//   // This line is a shortcut to tell TypeScript there is a
+//   // constant called `backpack`, and to not worry about where it came from.
+//   declare const backpack: Backpack<string>;
+//   // object is a string, because we declared it above as the variable part of Backpack.
+//   //result:backpack not define 
+//   const object = backpack.get();
+//   interface Point {
+//     x: number;
+//     y: number;
+//   }
+//   function logPoint(p: Point) {
+//     console.log(`${p.x}, ${p.y}`);
+//   }
+//   // logs "12, 26"
+//   //ts so sánh thấy có dạng giống nhau nên nó đã nhận Point làm kiểu dữ liệu của nó
+//   const point = { x: 12, y: 26 };
+//   logPoint(point);
+//   //error vì kiểu dữ liệu k trùng khớp 
+//   const point1={hex:"CCCCCC"};
+// //   logPoint(point1);
+//   //
+//   const rect = { x: 33, y: 3, width: 30, height: 80 };
+//   logPoint(rect); // logs "33, 3"
+console.log("Erased Structural Types");
+function logPoint(point) {
+    console.log("x = " + point.x + ", y = " + point.y);
+}
+function logName(x) {
+    console.log("Hello, " + x.name);
+}
+var obj = {
+    x: 0,
+    y: 0,
+    name: "Origin"
+};
+logPoint(obj);
+logName(obj);
